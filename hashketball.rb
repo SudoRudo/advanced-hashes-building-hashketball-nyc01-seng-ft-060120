9bb7c5d964af69427e7150f7aaff4adf525e53ce
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-17 06:47:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash
   {
     :home => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Brooklyn Nets",
+      :colors => [black, white],
       :players => [
         {player_name:"Alan Anderson", number:0, shoe:16, points:22, rebounds:12, assists:12, steals:3, blocks:1, slam_dunks:1},
         {player_name:"Reggie Evans", number:30, shoe:14, points:12, rebounds:12, assists:12, steals:12, blocks:12, slam_dunks:7},
@@ -13,8 +13,8 @@ def game_hash
     },
   
     :away => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => [turquoise, purple],
       :players => [
         {player_name:"Jeff Adrien", number:4, shoe:18, points:10, rebounds:1, assists:1, steals:2, blocks:7, slam_dunks:2},
         {player_name:"Bismack Biyombo", number:0, shoe:16, points:12, rebounds:4, assists:7, steals:22, blocks:15, slam_dunks:10},
